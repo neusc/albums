a0b3f2dc055867635b08a72b1d2991aae2682bf5
neusc <neusc829@163.com>
2018-03-07 17:35:30 +0800

    props.children的使用

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Created by chuans
+ * Date: 2018/3/7
+ */
+import React from 'react'
+import { View, Text } from 'react-native'
+import Card from './Card'
+import CardSection from './CardSection'
+
+const AlbumDetail = (props) => {
+  return (
+    <Card>
+      <CardSection>
+        <Text>{props.album.title}</Text>
+      </CardSection>
+    </Card>
+  )
+}
+
+export default AlbumDetail
