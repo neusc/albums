ea1e1eeee279a81cc005125c63484b167b160120
neusc <neusc829@163.com>
2018-03-08 18:26:40 +0800

    ScrollView组件的使用

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,7 +4,7 @@
  * Date: 2018/3/6
  */
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { ScrollView } from 'react-native'
 import axios from 'axios'
 import AlbumDetail from './AlbumDetail'
 
@@ -25,9 +25,9 @@ class AlbumList extends Component {
   render () {
     console.log(this.state)
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     )
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,13 @@
  * Date: 2018/3/7
  */
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 
-const Card = (props) => {
-  return (
-    <View style={styles.containerStyle}>
-      {props.children}
-    </View>
-  )
-}
+const Card = (props) => (
+  <View style={styles.containerStyle}>
+    {props.children}
+  </View>
+)
 
 const styles = {
   containerStyle: {
