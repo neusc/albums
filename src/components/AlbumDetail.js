e9ea992221ace7eab4c53cc416fa9d132d81c5d3
neusc <neusc829@163.com>
2018-03-08 18:06:25 +0800

    添加相册封面以及样式调整

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -10,22 +10,34 @@ import CardSection from './CardSection'
 
 const AlbumDetail = ({ album }) => {
   // destruct object
-  const { title, artist, thumbnail_image } = album
-  const { headerContentStyle, thumbStyle } = styles
+  const { title, artist, thumbnail_image, image } = album
+  const {
+    headerContentStyle,
+    thumbStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   )
@@ -36,9 +48,22 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbStyle: {
     width: 50,
     height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1
   }
 }
 
