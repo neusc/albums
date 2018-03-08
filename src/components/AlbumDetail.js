2c20a8adcbed06036aadd58fc7706f6ebfa9ab35
neusc <neusc829@163.com>
2018-03-08 17:21:35 +0800

    对象结构以及Image组件的使用

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,42 @@
  * Date: 2018/3/7
  */
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  // destruct object
+  const { title, artist, thumbnail_image } = album
+  const { headerContentStyle, thumbStyle } = styles
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image
+            style={thumbStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbStyle: {
+    width: 50,
+    height: 50
+  }
 }
 
 export default AlbumDetail
